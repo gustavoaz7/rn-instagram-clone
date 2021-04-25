2fd869421f50d2502f4ca210245744d93fa984c6
gustavoaz7 <gustavo_azevedo_7@hotmail.com>
2021-04-25 10:56:58 +0200

    chore - configures tests

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+const packagesToTranspile = ['react-native'];
+
+module.exports = {
+  preset: 'jest-expo',
+  setupFilesAfterEnv: ['./setup-tests.js'],
+  transformIgnorePatterns: [
+    `/node_modules/(?!${packagesToTranspile.join('|')})`,
+  ],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/*.{ts,tsx}',
+    '!**/coverage/**',
+    '!**/node_modules/**',
+    '!./App.tsx',
+  ],
+};
