0409f53ce71657f96a76a05ed67621757b670197
gustavoaz7 <gustavo_azevedo_7@hotmail.com>
2021-05-11 12:48:24 +0200

    feat - handles error case

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,20 +1,36 @@
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import AppLoading from 'expo-app-loading';
+import { Alert } from 'react-native';
 import { Navigation } from './navigation/Navigation';
 import { useAppDispatch } from './redux/hooks';
 import { userActions, userSelectors } from './redux/user';
 
 export const Root = (): JSX.Element => {
   const dispatch = useAppDispatch();
+  const login = useCallback(() => dispatch(userActions.getUser()), [dispatch]);
+
   useEffect(() => {
-    dispatch(userActions.getUser());
-  }, [dispatch]);
+    login();
+  }, [login]);
 
   const user = userSelectors.useUserSelector();
   const loading = userSelectors.useLoadingSelector();
   const error = userSelectors.useErrorSelector();
 
-  const isAppReady = !loading && !error && user;
+  if (error) {
+    Alert.alert(
+      'Failed to login.',
+      'Please make sure server is running and try again.',
+      [
+        {
+          text: 'Retry',
+          onPress: login,
+        },
+      ],
+    );
+  }
+
+  const isAppReady = !loading && user;
 
   return isAppReady ? <Navigation /> : <AppLoading />;
 };
