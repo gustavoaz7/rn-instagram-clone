fb03b49a265ae27acab4e3d0843f09cabbb41015
gustavoaz7 <gustavo_azevedo_7@hotmail.com>
2021-05-20 14:48:05 +0200

    chore - overrides nav theme & improves header style

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import { useTheme } from 'styled-components/native';
 import { RootStackNavigator } from './RootStackNavigator';
 import { navigationRef } from './navigation-ref';
 
 export function Navigation(): JSX.Element {
+  const theme = useTheme();
+  const navigationTheme = {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+      background: theme.color.white,
+    },
+  };
   return (
-    <NavigationContainer ref={navigationRef}>
+    <NavigationContainer ref={navigationRef} theme={navigationTheme}>
       <RootStackNavigator />
     </NavigationContainer>
   );
