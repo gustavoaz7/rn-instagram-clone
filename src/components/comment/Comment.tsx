26d262d59c1e76045f99ac677fd2083919e58940
gustavoaz7 <gustavo_azevedo_7@hotmail.com>
2021-06-21 21:40:51 +0200

    feat - wires comment like in comment

--- src/components/comment/Comment.tsx
+++ src/components/comment/Comment.tsx
@@ -1,14 +1,17 @@
-import React, { memo } from 'react';
-import styled from 'styled-components/native';
-import { ViewProps } from 'react-native';
+import React, { memo, useCallback, useMemo, useState } from 'react';
+import styled, { useTheme } from 'styled-components/native';
+import { ViewProps, Animated, Pressable } from 'react-native';
 import { Text } from '../text';
 import { AvatarWithRing } from '../avatar-with-ring';
 import HeartSvg from '../../../assets/svg/heart.svg';
 import { TComment } from '../../types';
 import { pluralizeWithS } from '../../utils/string';
 import { dateToString } from '../../utils/date';
+import { useAppDispatch } from '../../redux/hooks';
+import { commentsActions } from '../../redux/comments';
 
 export const AVATAR_SIZE = 40;
+const AnimatedPressable = Animated.createAnimatedComponent(Pressable);
 
 type TCommentProps = TComment & {
   interactable?: boolean;
@@ -16,13 +19,41 @@ type TCommentProps = TComment & {
 };
 
 export const Comment = memo(function Comment({
+  id,
   owner,
   previewLikes,
   createdAt,
   text,
+  viewerHasLiked,
   interactable = true,
   style,
 }: TCommentProps): JSX.Element {
+  const dispatch = useAppDispatch();
+  const theme = useTheme();
+  const [isLiked, setIsLiked] = useState(viewerHasLiked);
+  const heartScale = useMemo(() => new Animated.Value(1), []);
+
+  const dispatchLike = useCallback(
+    (flag: boolean) => {
+      dispatch(
+        commentsActions.likeComment({
+          flag,
+          id,
+        }),
+      );
+    },
+    [dispatch, id],
+  );
+
+  const handleLikeComment = useCallback(() => {
+    setIsLiked(prevIsLiked => {
+      dispatchLike(!prevIsLiked);
+      heartScale.setValue(theme.animation.heart.initialScale);
+      Animated.spring(heartScale, theme.animation.heart.springConfig).start();
+      return !prevIsLiked;
+    });
+  }, [dispatchLike, heartScale, theme.animation.heart]);
+
   return (
     <Container testID="Comment" style={style}>
       <AvatarWithRing
@@ -37,19 +68,31 @@ export const Comment = memo(function Comment({
         </CommentText>
         <Row>
           <CommentStats>{dateToString(new Date(createdAt), true)}</CommentStats>
-          {(interactable && previewLikes.count > 0 && (
+          {interactable ? (
             <>
-              <CommentStats>
-                {previewLikes.count}{' '}
-                {pluralizeWithS('like', previewLikes.count)}
-              </CommentStats>
+              {previewLikes.count > 0 ? (
+                <CommentStats>
+                  {previewLikes.count}{' '}
+                  {pluralizeWithS('like', previewLikes.count)}
+                </CommentStats>
+              ) : null}
               <CommentStats>Reply</CommentStats>
             </>
-          )) ||
-            null}
+          ) : null}
         </Row>
       </CommentContainer>
-      {interactable ? <HeartIcon /> : null}
+      {interactable ? (
+        <AnimatedPressable
+          onPress={handleLikeComment}
+          style={{ transform: [{ scale: heartScale }] }}
+        >
+          <HeartIcon
+            color={isLiked ? theme.color.red : theme.color.gray}
+            fill={isLiked ? theme.color.red : 'none'}
+            testID="Comment-Heart"
+          />
+        </AnimatedPressable>
+      ) : null}
     </Container>
   );
 });
@@ -83,8 +126,7 @@ const CommentText = styled(Text)`
   margin-bottom: ${({ theme }) => theme.spacing.xs};
 `;
 
-const HeartIcon = styled(HeartSvg).attrs(({ theme }) => ({
-  color: theme.color.gray,
+const HeartIcon = styled(HeartSvg).attrs(() => ({
   width: 16,
   height: 16,
 }))`
